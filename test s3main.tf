30efc8eef5e80f42200dbce6da65f828f89ae2db
LetoModelizer <LetoModelizer@no-reply.com>
2023-10-26 15:40:21 +0200

    leto-modelizer Thu Oct 26 2023

---
+++ test s3main.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "aws_s3_bucket" {
+    bucket = "bucket"
+}
+
+resource "aws_s3_bucket_acl" "aws_s3_bucket_acl" {
+    bucket = [
+        aws_s3_bucket.aws_s3_bucket.id,
+    ]
+    acl = "private"
+}
